20ff654eb91c870f51c338ee05a3c5759a923e96
rarothschild <rarothsc@gmail.com>
2022-12-30 13:11:48 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# HouseHold2
+ 
---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { currentUser } from './lib/pocketbase'
+  import Login from './lib/Login.svelte';
+  import Residences from './lib/Residences.svelte';
+</script>
+
+<main>
+  <h1>Welcome to Household</h1>
+  {#if $currentUser}
+    <Residences />
+  {:else}
+    <Login />
+  {/if}
+  
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>
